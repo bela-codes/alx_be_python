a44389948449c6a3e493f7a2344b056681b0f3d2
Abel Tesfaye <abel.tesfaye.codes@gmail.com>
2025-07-30 23:08:48 +0300

    Modified

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,3 +1,24 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+    celsius = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
+
+def convert_to_fahrenheit(celsius):
+    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
+    return fahrenheit
+
+measurement = int(input("Enter the magnitude of your measurement: "))
+unit = input("Is this temprature in Celsius or Fahrenheit? (C/F): ").lower()
+
+if unit == "c":
+    converted = convert_to_fahrenheit(measurement)
+    print(f"{measurement}°C is {converted}°F")
+elif unit == "f":
+    converted = convert_to_celsius(measurement)
+    print(f"{measurement}°F is {converted}°C")
+
 # FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 # CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
@@ -20,25 +41,3 @@
 #     print(f"{magnitude}°F is {converted_to_celsius}°C")
 # else:
 #     print(f"please choose a valid option")
-
-
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-
-def convert_to_celsius(fahrenheit):
-    celsius = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
-    return celsius
-
-def convert_to_fahrenheit(celsius):
-    fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
-    return fahrenheit
-
-measurement = int(input("Enter the magnitude of your measurement: "))
-unit = input("Is this temprature in Celsius or Fahrenheit? (C/F): ").lower()
-
-if unit == "c":
-    converted = convert_to_fahrenheit(measurement)
-    print(f"{measurement}°C is {converted}°F")
-elif unit == "f":
-    converted = convert_to_celsius(measurement)
-    print(f"{measurement}°F is {converted}°C")
