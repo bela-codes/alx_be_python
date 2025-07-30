87f410f234bb964412e70153b15acb9ce54e93cc
Abel Tesfaye <abel.tesfaye.codes@gmail.com>
2025-07-30 22:58:48 +0300

    Modified

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,29 @@
-# THIS ARE THE MULTIPLICATION FACTORS
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+# FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+# CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+# def convert_to_celsius(fahrenheit):
+#     celsius = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
+#     return celsius
+
+# def convert_to_fahrenheit(celsius):
+#     fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
+#     return fahrenheit
+
+# magnitude = int(input("Enter the magnitude: "))
+# user = input("Enter the unit of your measurement(Celsius(C), Fahrenheit(F)): ").lower()
+
+# if user == "c":
+#     converted_to_fahrenheit = convert_to_fahrenheit(magnitude)
+#     print(f"{magnitude}°C is {converted_to_fahrenheit}°F")
+# elif user == "f":
+#     converted_to_celsius = convert_to_celsius(magnitude)
+#     print(f"{magnitude}°F is {converted_to_celsius}°C")
+# else:
+#     print(f"please choose a valid option")
+
+
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     celsius = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
@@ -10,15 +33,12 @@ def convert_to_fahrenheit(celsius):
     fahrenheit = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
     return fahrenheit
 
-magnitude = int(input("Enter the magnitude: "))
-user = input("Enter the unit of your measurement(Celsius(C), Fahrenheit(F)): ").lower()
+measurement = int(input("Enter the magnitude of your measurement: "))
+unit = input("Is this temprature in Celsius or Fahrenheit? (C/F): ").lower()
 
-if user == "c":
-    converted_to_fahrenheit = convert_to_fahrenheit(magnitude)
-    print(f"{magnitude}°C is {converted_to_fahrenheit}°F")
-elif user == "f":
-    converted_to_celsius = convert_to_celsius(magnitude)
-    print(f"{magnitude}°F is {converted_to_celsius}°C")
-else:
-    print(f"please choose a valid option")
-    
+if unit == "c":
+    converted = convert_to_fahrenheit(measurement)
+    print(f"{measurement}°C is {converted}°F")
+elif unit == "f":
+    converted = convert_to_celsius(measurement)
+    print(f"{measurement}°F is {converted}°C")
